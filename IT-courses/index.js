0dab524cb1bfc3e70570d031c0c2562d0fdb2144
Anastasia Deryabkina <der_anastasi84@mail.ru>
2023-02-07 13:11:00 +0700

    Fixed the burger menu view. Added a color change when the menu is converted to a burger and back.

--- IT-courses/index.js
+++ IT-courses/index.js
@@ -3,13 +3,10 @@ let menu_items = document.getElementsByTagName("li");
 
 for(let item = 0; item < menu_items.length; item++) 
 {
-    menu_items[item].addEventListener('click', e => changeActive(e));
-    console.log(menu_items[item]);
-};
-// console.log(menu_items);
+    menu_items[item].addEventListener('click', e => changeActive(e))
+}
 
 function changeActive(e) {
     document.getElementsByClassName("menu-item-active")[0].classList.remove("menu-item-active");
-    console.log(e);
-    e.srcElement.classList.add("menu-item-active");
+    e.srcElement.classList.add("menu-item-active")
 }
